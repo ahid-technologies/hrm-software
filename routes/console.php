20f2db5443579fb6e99ee02ee12f8f7a68be4cdb
Ahid Technologies <ahidtechnologies@gmail.com>
2025-09-09 23:23:54 +0500

    Remove unused 'inspire' command from console routes

--- routes/console.php
+++ routes/console.php
@@ -1,11 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 Artisan::command('hr:check-expiries', function () {
     $this->call('hr:check-expiries');

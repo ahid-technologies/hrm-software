dfbd9a86e3fe4e2bb9b0f70e1138ed0b49d1ba22
Ahid Technologies <ahidtechnologies@gmail.com>
2025-09-09 23:21:28 +0500

    Update footer links to use dynamic application name for support requests

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,28 +4,28 @@
             <div class="col-lg-auto ms-lg-auto d-none d-md-block">
                 <ul class="list-inline list-inline-dots mb-0">
                     <li class="list-inline-item">
-                        <a href="mailto:info@ahidtechnologies.com?subject=Support Request - Auto Shop HR&body=Hello, I need support with the Auto Shop HR system.%0D%0A%0D%0APlease describe your issue:"
+                        <a href="mailto:info@ahidtechnologies.com?subject=Support Request - {{ config('app.name', 'HR System') }}&body=Hello, I need support with the {{ config('app.name', 'HR System') }} system.%0D%0A%0D%0APlease describe your issue:"
                             class="link-secondary" title="Get Support">
                             <i class="ti ti-help-circle me-1"></i>
                             Support
                         </a>
                     </li>
                     <li class="list-inline-item">
-                        <a href="mailto:info@ahidtechnologies.com?subject=Feature Request - Auto Shop HR&body=Hello, I would like to request a new feature for the Auto Shop HR system.%0D%0A%0D%0AFeature Description:%0D%0A%0D%0ABusiness Justification:%0D%0A%0D%0AUrgency Level:"
+                        <a href="mailto:info@ahidtechnologies.com?subject=Feature Request - {{ config('app.name', 'HR System') }}&body=Hello, I would like to request a new feature for the {{ config('app.name', 'HR System') }} system.%0D%0A%0D%0AFeature Description:%0D%0A%0D%0ABusiness Justification:%0D%0A%0D%0AUrgency Level:"
                             class="link-secondary" title="Request New Feature">
                             <i class="ti ti-bulb me-1"></i>
                             Request Feature
                         </a>
                     </li>
                     <li class="list-inline-item">
-                        <a href="mailto:info@ahidtechnologies.com?subject=Bug Report - Auto Shop HR&body=Hello, I found a bug in the Auto Shop HR system.%0D%0A%0D%0ASteps to reproduce:%0D%0A1. %0D%0A2. %0D%0A3. %0D%0A%0D%0AExpected behavior:%0D%0A%0D%0AActual behavior:%0D%0A%0D%0ABrowser/Device info:"
+                        <a href="mailto:info@ahidtechnologies.com?subject=Bug Report - {{ config('app.name', 'HR System') }}&body=Hello, I found a bug in the {{ config('app.name', 'HR System') }} system.%0D%0A%0D%0ASteps to reproduce:%0D%0A1. %0D%0A2. %0D%0A3. %0D%0A%0D%0AExpected behavior:%0D%0A%0D%0AActual behavior:%0D%0A%0D%0ABrowser/Device info:"
                             class="link-secondary" title="Report Bug">
                             <i class="ti ti-bug me-1"></i>
                             Report Bug
                         </a>
                     </li>
                     <li class="list-inline-item">
-                        <a href="https://wa.me/923020824146?text=Hello, I need assistance with the Auto Shop HR system."
+                        <a href="https://wa.me/923020824146?text=Hello, I need assistance with the {{ config('app.name', 'HR System') }} system."
                             target="_blank" class="link-success" title="WhatsApp Support">
                             <i class="ti ti-brand-whatsapp me-1"></i>
                             WhatsApp
